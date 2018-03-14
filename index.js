b7347fda0449bd7b5e88f9aaf69cceaab6e49945
Michael Roth <mick2090@gmail.com>
2018-03-14 14:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function whisper(string){
   console.log(string.toLowerCase);
 }
 
-function sayHiToGrandma(string){
-  if(string.toLowerCase === string){
-    return "I can't hear you!"
-  }else if(string.toUpperCase){
-    return "YES INDEED!";
-  }else if(string === "I love you, Grandma"){
-    return "I love you, too."
-  }
-}
+// function sayHiToGrandma(string){
+//   if(string.toLowerCase === string){
+//     return "I can't hear you!"
+//   }else if(string.toUpperCase){
+//     return "YES INDEED!";
+//   }else if(string === "I love you, Grandma"){
+//     return "I love you, too."
+//   }
+// }
 
